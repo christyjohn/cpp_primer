88aae7dcdb35a68a284e54ce61c4bcbbf36aaee4
Christy John <christyjohn.crz@gmail.com>
2020-03-27 23:24:07 +0530

    string exercises

---
+++ ch03_strings_vectors_arrays/string_io.cc
@@ -0,0 +1,17 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    string s;;
+
+    cin >> s;
+    cout << s << endl;
+    
+    return 0;
+}
