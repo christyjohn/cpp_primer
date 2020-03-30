1e997e4cd84f294869a609dbf501d0780a3b5143
Christy John <christyjohn.crz@gmail.com>
2020-03-30 19:02:59 +0530

    starting iterators

---
+++ ch03_strings_vectors_arrays/iter_capitalize_first.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include <string>
+using std::string;
+
+int main()
+{
+    string s("some string");
+
+    if (s.begin() != s.end()) {
+        auto it = s.begin();
+        *it = toupper(*it);
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
