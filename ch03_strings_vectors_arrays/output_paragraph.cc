a9718d040df12b111fda5b63353c080ca1b6c3ce
Christy John <christyjohn.crz@gmail.com>
2020-03-31 18:53:06 +0530

    practicing iterators

---
+++ ch03_strings_vectors_arrays/output_paragraph.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+#include<string>
+using std::string;
+
+#include<vector>
+using std::vector;
+
+int main()
+{
+    vector<string> text = {"Lorem ipsum dolor sit amet,", "consectetur adipiscing elit,",
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", " ",
+        "Ut enim ad minim veniam, ","quis nostrud exercitation ullamco laboris", 
+        "nisi ut aliquip ex ea commodo consequat. "};
+
+    for (auto it = text.cbegin(); it !=text.cend() && !it -> empty(); ++it)
+        cout <<*it << endl; 
+
+}
